786f1d2c919e2af9e2bd3cf9c3c695d3a38747b6
Martynov Andrey <martynov.a@routeamgroup.com>
2022-05-17 17:28:44 +0300

    Нарисовал виджет погоды

--- 14_advanced_widgets/lib/pages/home_page.dart
+++ 14_advanced_widgets/lib/pages/home_page.dart
@@ -1,101 +1,187 @@
 import 'package:advanced_widgets/common/theme.dart';
 import 'package:advanced_widgets/common/theme_switcher.dart';
+import 'package:advanced_widgets/pages/weather_indicator.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key, required this.title}) : super(key: key);
-
   final String title;
+  const HomePage({Key? key, required this.title}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  ThemeData? currentTheme;
+  bool isShowDetailedWeather = false;
+  double opacityVisible = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor:
+          ThemeSwitcher.of(context)?.themeModel.theme.backgroundColor,
       appBar: AppBar(
         centerTitle: true,
         title: Text(widget.title),
+        backgroundColor: ThemeSwitcher.of(context)
+            ?.themeModel
+            .theme
+            .appBarTheme
+            .backgroundColor,
       ),
       drawer: Drawer(
         //backgroundColor: Theme.of(context).backgroundColor,
+        child: Column(
+          children: <Widget>[
+            DrawerHeader(
+              child: CircleAvatar(
+                radius: 50,
+                backgroundColor:
+                    ThemeSwitcher.of(context)?.themeModel.theme.backgroundColor,
+              ),
+            ),
+            ListTile(
+              title: const Text('Green'),
+              trailing: const Icon(
+                Icons.invert_colors,
+                color: Colors.green,
+              ),
+              onTap: () {
+                ThemeSwitcher.of(context)?.themeModel.theme = greenThemeLight();
+                setState(() {});
+                print('Green');
+              },
+            ),
+            ListTile(
+              title: const Text('Brown'),
+              trailing: const Icon(
+                Icons.invert_colors,
+                color: Colors.brown,
+              ),
+              onTap: () {
+                ThemeSwitcher.of(context)?.themeModel.theme = brownThemeLight();
+                setState(() {});
 
-        child: Builder(
-          builder: (innerContext) => Column(
-            children: <Widget>[
-              DrawerHeader(
-                child: CircleAvatar(
-                  radius: 50,
-                  backgroundColor: Theme.of(context).backgroundColor,
-                ),
+                print('Brown');
+              },
+            ),
+            ListTile(
+              title: const Text('Purple'),
+              trailing: const Icon(
+                Icons.invert_colors,
+                color: Colors.deepPurple,
               ),
-              ListTile(
-                title: const Text('Green'),
-                trailing: const Icon(
-                  Icons.invert_colors,
-                  color: Colors.green,
-                ),
-                onTap: () {
-                  setState(() {
-                    var currentTheme = deepPurpleThemeLight();
-                  });
-                  print('Green');
-                },
+              onTap: () {
+                setState(() {
+                  ThemeSwitcher.of(context)?.themeModel.theme =
+                      deepPurpleThemeLight();
+                });
+                print('Purple');
+              },
+            ),
+            ListTile(
+              title: const Text('Indigo'),
+              trailing: const Icon(
+                Icons.invert_colors,
+                color: Colors.indigo,
               ),
-              ListTile(
-                title: const Text('Brown'),
-                trailing: const Icon(
-                  Icons.invert_colors,
-                  color: Colors.brown,
-                ),
-                onTap: () {
-                  print('Brown');
-                },
+              onTap: () {
+                setState(() {
+                  ThemeSwitcher.of(context)?.themeModel.theme =
+                      indigoThemeLight();
+                });
+                print('Indigo');
+              },
+            ),
+            ListTile(
+              title: const Text('Dark'),
+              trailing: const Icon(
+                Icons.invert_colors,
+                color: Colors.black,
               ),
-              ListTile(
-                title: const Text('Purple'),
-                trailing: const Icon(
-                  Icons.invert_colors,
-                  color: Colors.deepPurple,
-                ),
-                onTap: () {
-                  print('Purple');
-                },
-              ),
-              ListTile(
-                title: const Text('Indigo'),
-                trailing: const Icon(
-                  Icons.invert_colors,
-                  color: Colors.indigo,
-                ),
-                onTap: () {
-                  print('Indigo');
-                },
-              ),
-              ListTile(
-                title: const Text('Dark'),
-                trailing: const Icon(
-                  Icons.invert_colors,
-                  color: Colors.black,
-                ),
-                onTap: () {
-                  print('Dark');
-                },
-              ),
-            ],
-          ),
+              onTap: () {
+                setState(() {
+                  ThemeSwitcher.of(context)?.themeModel.theme = themeDark;
+                });
+                print('Dark');
+              },
+            ),
+          ],
         ),
       ),
-      body: Center(
-        child: Text(
-          'You have pushed the button this many times:',
-          style: Theme.of(context).textTheme.bodyText1,
+      body: Padding(
+        padding:
+            const EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 70),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  isShowDetailedWeather = !isShowDetailedWeather;
+                });
+              },
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: isShowDetailedWeather
+                    ? Column(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          SizedBox(
+                            height: 80,
+                            width: 80,
+                            child: WeatherIndicator(
+                                opacityVisible: opacityVisible),
+                          ),
+                        ],
+                      )
+                    : Column(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: 200,
+                            width: 200,
+                            child: WeatherIndicator(
+                                opacityVisible: opacityVisible),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const Text('Облачно'),
+                          const Text('12°'),
+                        ],
+                      ),
+              ),
+            ),
+            Slider(
+              activeColor: Colors.grey[300],
+              min: 0.0,
+              max: 1.0,
+              label: '$opacityVisible',
+              divisions: 10,
+              value: opacityVisible,
+              onChanged: (v) {
+                setState(() {
+                  opacityVisible = v;
+                });
+              },
+            )
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
+        backgroundColor: ThemeSwitcher.of(context)
+            ?.themeModel
+            .theme
+            .floatingActionButtonTheme
+            .backgroundColor,
         onPressed: () {},
       ),
     );
--- 14_advanced_widgets/lib/pages/weather_indicator.dart
+++ 14_advanced_widgets/lib/pages/weather_indicator.dart
@@ -1,39 +1,183 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
-class WeatherIndicator extends CustomPainter {
+class WeatherIndicator extends StatelessWidget {
+  final double opacityVisible;
+
+  const WeatherIndicator({Key? key, required this.opacityVisible})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double _getSunOpacity(double value) {
+      if (value > 0.3) {
+        return 0;
+      }
+      return 10 / 3 * value;
+    }
+
+    // double _getCloudOpacity(double value) {
+    //   if (value <= 0.3 && value >= 0.7) {
+    //     return 0;
+    //   }
+    //   return 10 / 3 * value - 7 / 3;
+    // }
+
+    double _getDropsOpacity(double value) {
+      if (value < 0.7) {
+        return 0;
+      }
+      return 10 / 3 * value - 7 / 3;
+    }
+
+    print('opacityVisible!!!: $opacityVisible');
+    print('Sun Opacity: ${_getSunOpacity(opacityVisible)}');
+    //print('Cloud Opacity: ${_getCloudOpacity(opacityVisible)}');
+    print('Drops Opacity: ${_getDropsOpacity(opacityVisible)}');
+    return Stack(
+      children: [
+        Opacity(
+          opacity: _getSunOpacity(opacityVisible),
+          child: SizedBox(
+            height: double.infinity,
+            width: double.infinity,
+            child: CustomPaint(
+              painter: Paint1(),
+            ),
+          ),
+        ),
+        Opacity(
+          opacity: 1,
+          child: SizedBox(
+            height: double.infinity,
+            width: double.infinity,
+            child: CustomPaint(
+              painter: Paint2(),
+            ),
+          ),
+        ),
+        Opacity(
+          opacity: _getDropsOpacity(opacityVisible),
+          child: SizedBox(
+            height: double.infinity,
+            width: double.infinity,
+            child: CustomPaint(
+              painter: Paint3(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+double _degreeToRadians(num degree) {
+  return (degree * math.pi) / 180.0;
+}
+
+class Paint1 extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     final painter1 = Paint()
-      ..color = Colors.yellow
+      ..color = Colors.yellow.shade300
       ..style = PaintingStyle.fill;
 
+    //солнце
+    var path1 = Path();
+    path1.addOval(Rect.fromCircle(
+      center: Offset(size.width * .35, size.height * .25),
+      radius: size.width * .3,
+    ));
+    canvas.drawPath(path1, painter1);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+}
+
+class Paint2 extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
     final painter2 = Paint()
-      ..color = Colors.grey.shade300 //.withOpacity(0.5)
+      ..color = Colors.grey.shade300
       ..style = PaintingStyle.fill;
 
+    //облако
+    var path2 = Path()
+      ..moveTo(size.width * .1, size.height * .65)
+      ..arcToPoint(
+        Offset(
+          size.width * .1,
+          size.height * .45,
+        ),
+        clockwise: true,
+        radius: Radius.circular(size.height * .05),
+      )
+      ..quadraticBezierTo(
+        size.width * .2,
+        size.height * .2,
+        size.width * .5,
+        size.height * .3,
+      )
+      ..arcToPoint(
+        Offset(
+          size.width * .9,
+          size.height * .45,
+        ),
+        clockwise: true,
+        radius: Radius.circular(size.height * .03),
+      )
+      ..arcToPoint(
+        Offset(
+          size.width * .9,
+          size.height * .65,
+        ),
+        clockwise: true,
+        radius: Radius.circular(size.height * .03),
+      )
+      ..close();
+
+    canvas.drawPath(path2, painter2);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+}
+
+class Paint3 extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
     final painter3 = Paint()
-      ..color = Colors.grey.shade800 //.withOpacity(0.5)
+      ..color = Colors.grey.shade800
       ..style = PaintingStyle.fill;
 
     final painter4 = Paint()
       ..color = Colors.blue.shade500
       ..style = PaintingStyle.fill;
 
-    var path1 = Path();
-    path1.addOval(Rect.fromCircle(
-      center: Offset(size.width / 2, size.height / 2),
-      radius: 50,
-    ));
-    canvas.drawPath(path1, painter1);
+    //капли
+    var path3 = Path()
+      ..moveTo(size.width * .8, size.height * .7)
+      ..lineTo(
+        size.width * .8,
+        size.height * .75,
+      )
+      ..arcToPoint(
+        Offset(
+          size.width * .78,
+          size.height * .745,
+        ),
+        clockwise: true,
+        radius: Radius.circular(size.height * .002),
+      );
 
-    var path2 = Path();
-    path2.addOval(Rect.fromCircle(
-      center: Offset(size.width * 2, size.height / 2),
-      radius: 25,
-    ));
-    canvas.drawPath(path2, painter2);
+    path3.addPath(path3, Offset(size.width * .1, size.height * .1));
+    path3.addPath(path3, Offset(size.width * -.1, size.height * .1));
+    path3.addPath(path3, Offset(size.width * -.2, 0));
+    path3.addPath(path3, Offset(size.width * -.4, 0));
+    canvas.drawPath(path3, painter4);
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
