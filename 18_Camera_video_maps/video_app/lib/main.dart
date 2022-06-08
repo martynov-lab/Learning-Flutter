835012c34d0c9e53ff288fbe6b075ed479b9d4c8
Martynov Andrey <martynov.a@routeamgroup.com>
2022-06-08 17:03:59 +0300

    #18.5.2 Добавил плеер для проигрывания видео из assets

--- 18_Camera_video_maps/video_app/lib/main.dart
+++ 18_Camera_video_maps/video_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,13 +30,77 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late VideoPlayerController _videoPlayerController;
+  @override
+  void initState() {
+    super.initState();
+    _videoPlayerController = VideoPlayerController.asset('assets/01.mp4');
+    // _videoPlayerController = VideoPlayerController.network(
+    //     'https://www.youtube.com/watch?v=JRuTofOvhiE' /*, closedCaptionFile: _loadCaptions()*/);
+
+    _videoPlayerController.addListener(() {
+      setState(() {});
+    });
+    _videoPlayerController.setLooping(true);
+    _videoPlayerController.initialize().then((value) => setState(() {}));
+    _videoPlayerController.play();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _videoPlayerController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(),
+      body: SizedBox(
+          height: 200,
+          child: Stack(
+            children: [
+              VideoPlayer(_videoPlayerController),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: VideoProgressIndicator(
+                  _videoPlayerController,
+                  allowScrubbing: false,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 16,
+                  bottom: 16,
+                ),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                    child: Row(mainAxisSize: MainAxisSize.min, children: [
+                      IconButton(
+                        onPressed: () {
+                          _videoPlayerController.pause();
+                        },
+                        icon: const Icon(Icons.pause),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          _videoPlayerController.play();
+                        },
+                        icon: const Icon(Icons.play_arrow),
+                      ),
+                    ]),
+                  ),
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
