250c497935e2d53c511b15f195154f0b826da7f1
Martynov Andrey <martinov-studio@yandex.ru>
2022-07-11 22:06:33 +0300

    18.5.4 Создал контроллер. Сделал кнопки.

--- 18_Camera_video_maps/browser_app/lib/main.dart
+++ 18_Camera_video_maps/browser_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -27,12 +28,132 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late WebViewController _controller;
+  final CookieManager _cookieManager = CookieManager();
+  String urlValur = '';
+  bool isLoading = true;
+  @override
+  void initState() {
+    super.initState();
+    WebView.platform = SurfaceAndroidWebView();
+  }
+
+  void _loadURL(String url) {
+    setState(() {
+      isLoading = true;
+    });
+    _controller.loadUrl(url).whenComplete(() {
+      setState(() {
+        isLoading = false;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.home),
+          onPressed: () {
+            _loadURL('https://google.com/');
+          },
         ),
-        body: const Center());
+        title: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 5),
+                child: GestureDetector(
+                  child: const Icon(Icons.navigate_before),
+                  onTap: () {
+                    _controller.goBack();
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 5),
+                child: isLoading
+                    ? GestureDetector(
+                        child: const Icon(Icons.replay),
+                        onTap: () {
+                          _controller.reload();
+                          setState(() {
+                            isLoading = true;
+                          });
+                        },
+                      )
+                    : GestureDetector(
+                        child: const Icon(Icons.close),
+                        onTap: () {
+                          //_controller.;
+                          setState(() {
+                            isLoading = false;
+                          });
+                        },
+                      ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: GestureDetector(
+                  child: const Icon(Icons.navigate_next),
+                  onTap: () {
+                    _controller.goForward();
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  alignment: Alignment.centerLeft,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.white,
+                  ),
+                  child: TextField(
+                    cursorColor: Colors.grey,
+                    autocorrect: false,
+                    maxLines: 1,
+                    autofocus: true,
+                    onChanged: (value) {
+                      setState(() {
+                        urlValur = value;
+                      });
+                    },
+                    onEditingComplete: () {
+                      _loadURL(urlValur);
+                      FocusScope.of(context).requestFocus(FocusNode());
+                    },
+                    decoration: const InputDecoration(
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                      hintStyle: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 88, 95, 130), width: 2),
+                      ),
+                      hintText: 'Введите URL',
+                    ),
+                  ),
+                ),
+              ),
+            ]),
+      ),
+      body: WebView(
+        initialUrl: 'https://google.com/',
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (controller) {
+          _controller = controller;
+        },
+      ),
+    );
   }
 }
